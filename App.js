3b86693271678f4774b8fa8e527cb8c6e4619f23
neal.bharucha@msichicago.org <nealbharucha@yahoo.com>
2018-05-11 10:46:59 -0500

    add TextInput

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+    placeName: ''
+  };
+};
+
+  placeNameChangedHandler = (val) => {
+    this.setState({
+      placeName: val
+    });
+  };
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>Neal's First Real Commit. Android App.</Text>
+        <TextInput
+          style={{ width: 300 }}
+          value={this.state.placeName}
+          onChange={this.placeNameChangedHandler}
+          placeholder='Cool Place Here...'
+        />
       </View>
     );
   }
@@ -14,8 +32,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
